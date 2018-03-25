1e85aa06e6cb46f5f2a20c8519aafa42677b4d83
angel <angel.ignacio.colmenares@gmail.com>
2018-03-25 16:22:51 -0500

    mostrar FPS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 
 export class AppComponent 
 {  
+
+  fps:number=0.00;
+  handleImageSuccess(elapsed):void
+  {
+    if(elapsed!=0)
+      this.fps = (1000.00/elapsed).toFixed(2);
+    else
+      this.fps=0.00;  
+  }
+
+
 }
